43060987fa3b1d2095cd80054af47ef762301893
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-07-26 16:43:42 +0530

    recent commt

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,14 @@ const db = mysql.createConnection({
   database: "curdschema",
 });
 
-
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM bookstore";
   db.query(q, (err, data) => {
     console.log("SQL connected");
     if (err) {
       return res.json({
-        error:true,
-        message:`Error in getting books ${err}`
+        error: true,
+        message: `Error in getting books ${err}`,
       });
     }
     return res.json(data);
@@ -48,15 +47,16 @@ app.post("/books", (req, res) => {
     });
   }
   const q = "INSERT INTO bookstore (`title`, `desc`, `cover`) VALUES (?, ?, ?)";
-  const values = [title, desc, cover ];
+  const values = [title, desc, cover];
 
   db.query(q, values, (err, data) => {
-    if (err) return res.json({
-      error: true,
-      message: `error in post books ${err}`,
-    });
+    if (err)
+      return res.json({
+        error: true,
+        message: `error in post books ${err}`,
+      });
     return res.json({
-      error:false,
+      error: false,
       message: "Books stored successfully",
       data: data,
     });
@@ -64,13 +64,13 @@ app.post("/books", (req, res) => {
 });
 
 app.put("/books/:id", (req, res) => {
-   const bookId = req.params.id;
-   const title = req.body.title;
-   const desc = req.body.desc;
-   const cover = req.body.cover;
-   let q = "UPDATE bookstore SET";
-     const values = [];
-  if(title) {
+  const bookId = req.params.id;
+  const title = req.body.title;
+  const desc = req.body.desc;
+  const cover = req.body.cover;
+  let q = "UPDATE bookstore SET";
+  const values = [];
+  if (title) {
     q += ` title = ${JSON.stringify(title)} WHERE id = ${bookId} `;
   }
   if (desc) {
@@ -80,13 +80,13 @@ app.put("/books/:id", (req, res) => {
     q += ` cover = ${JSON.stringify(cover)} WHERE id = ${bookId} `;
   }
 
- 
   db.query(q, (err, data) => {
-    console.log("updatedquery: " + q)
-    if (err) return res.json({
-      error: true,
-      message: `Invalid request check the request body or bookId`,
-    });
+    console.log("updatedquery: " + q);
+    if (err)
+      return res.json({
+        error: true,
+        message: `Invalid request check the request body or bookId`,
+      });
 
     if (data.affectedRows === 0) {
       return res.json({
@@ -95,45 +95,48 @@ app.put("/books/:id", (req, res) => {
       });
     }
 
-    if(title){
+    if (title) {
       return res.json({
-        error:false,
+        error: false,
         message: "Book title updated",
         data: data,
       });
     }
     if (desc) {
       return res.json({
-        error:false,
+        error: false,
         message: "Book desc updated",
         data: data,
       });
     }
     if (cover) {
       return res.json({
-        error:false,
+        error: false,
         message: "Book cover updated",
         data: data,
       });
     }
-     return res.json({
-       error: true,
-       message: "Not updated need to provide any value",
-     });
-   
+    return res.json({
+      error: true,
+      message: "Not updated need to provide any value",
+    });
   });
 });
 
-app.delete('/books/:id',(req,res)=>{
-  const bookId= req.params.id;
-  let q=`DELETE FROM bookstore where ID=${bookId}`;
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  let q = `DELETE FROM bookstore where ID=${bookId}`;
   console.log("bookId: " + bookId);
-  db.query(q,(err,data)=>{
-    if(err) return res.json({error:true, message:err});
-    console.log('deleted successfully'); 
-    return res.json({error:false, message:"Deleted successfully",data:data}) 
-  })
-})
+  db.query(q, (err, data) => {
+    if (err) return res.json({ error: true, message: err });
+    console.log("deleted successfully");
+    return res.json({
+      error: false,
+      message: "Deleted successfully",
+      data: data,
+    });
+  });
+});
 
 app.get("/", (req, res) => {
   res.json("Hello World!");
